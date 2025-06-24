d36c0090152e298b94fd2090f43657c956275908
linuxunil <linuxunil@gmail.com>
2025-06-24 16:36:20 -0500

    Load API KEY form .env

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+# This library gives us access to Operating System utilites, ie. loading the env variables.
+import os
+
+from dotenv import load_dotenv
+
+# This function reads the .env file and gives us environment variables
+load_dotenv()
+
 def main():
-    print("Hello from movieexporter!")
+    # Loads API KEY from .env file
+    API_KEY = os.getenv("OMDB_API_KEY")
+    print("Hello from Movie Exporter!")
 
 
 if __name__ == "__main__":
